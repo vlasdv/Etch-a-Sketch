96f48b1a68003696ffbdb73a040b03807342b69d
Dmitrii Vlasov <vlas.dv@gmail.com>
2023-11-07 20:31:45 +0100

    Add function for draw a grid, using #grid-size value

---
+++ static/app.js
@@ -0,0 +1,17 @@
+const container = document.querySelector('.container');
+let gridSize = Number(document.querySelector('#grid-size').value);
+
+function drawGrid() {
+  for (let row = 0; row < gridSize; row++) {
+    let newRow = document.createElement('div');
+    newRow.className = 'row';
+    for (let col = 0; col < gridSize; col++) {
+      let newCol = document.createElement('div');
+      newCol.className = 'cell';
+      newRow.appendChild(newCol);
+    }
+    container.appendChild(newRow);
+  }
+}
+
+drawGrid();
